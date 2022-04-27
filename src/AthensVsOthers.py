28139f800c7ab760a25bf2be77d6476042c82ed5
judyatohio <js467820@ohio.edu>
2022-04-27 00:05:01 -0400

    done with AthensVsOthers

--- src/AthensVsOthers.py
+++ src/AthensVsOthers.py
@@ -13,63 +13,130 @@ import pandas
 #import numpy
 import matplotlib.pyplot as plot
 
-# global constants for the column identifiers
-#FIRST_NAME = 6
-#LAST_NAME = 8
-COUNTY = 15
-AMOUNT = 17
-
-def run(fileName):
-    #importing our excel sheet 
-    df = pandas.read_excel(fileName, header = 1)
-    columns = df.values.T.tolist()
-
-    total = 0
-    dTotal = 0
-    # get the amounts from athens added up
-    for i in range(len(columns[AMOUNT])):
-        # county and amount import, with NaN protection on the amount
-        county = str(columns[COUNTY][i])
-
-
-        if pandas.isna(columns[AMOUNT][i]):
-            amount = 0
+def formatName(name):
+    temp = ""
+    i = 0
+    while i < len(name):
+        if i == 0 or name[i - 1] == " ":
+            temp += name[i].upper()
         else:
-            amount = int(columns[AMOUNT][i])
+            temp += name[i].lower()
+        i += 1
 
-        # format it to 5 digits
-        if len(county) == 6:
-            # remove the comma
-            county.split(",")
-            temp = ""
-            for i in county:
-                temp += i
-            county = temp
-        elif len(county) > 6:
-            # remove everything after the first five digits
-            county[0:5]
+    return temp
 
-        # add amount to total if county is athens
-        if county == "45701":
-            total += amount
-            dTotal += 1
+def run(files):
+    #importing our excel sheet
 
-    # print the info we got
-    print(f"{amount} donated from athens county, in {dTotal} donations.")
+    # these two need to go in all functions
+    count = 0
+    labels = []
 
-    """
-    # make the pie chart
+    # data arrays
+    total = []
+    totalFromAthens = []
+    dTotal = []
+
+    for fileName in files:
+        # deciding which columns to look at
+        race = fileName.split("/")[2]
+        if race == "attorney-race":
+            COUNTY = 8
+            AMOUNT = 12
+            HEAD = 2
+        elif race == "sec-of-state-race":
+            COUNTY = 8
+            AMOUNT = 12
+            HEAD = 2
+        elif race == "treasurer-race":
+            COUNTY = 8
+            AMOUNT = 12
+            HEAD = 1
+        elif race == "auditor-race":
+            if fileName.split("/")[3] == "keith-faber":
+                COUNTY = 15
+                AMOUNT = 17
+            elif fileName.split("/")[3] == "taylor-sappington":
+                COUNTY = 8
+                AMOUNT = 12
+            HEAD = 1
+        elif race == "gov-races":
+            COUNTY = 15
+            AMOUNT = 17
+            HEAD = 2
+        elif race == "sen-races":
+            COUNTY = 25
+            AMOUNT = 35
+            HEAD = 1
+        elif race == "house-races":
+            COUNTY = 8
+            AMOUNT = 12
+            HEAD = 1
+        
+        #actually reading the file
+        df = pandas.read_excel(fileName, header = HEAD)
+        columns = df.values.T.tolist() #makes an array of the spreadsheet
+
+        #building the labels
+        fileName = fileName.split("/")
+        labels.append(fileName[3].split("-"))   
+        labels[count] = labels[count][1]
+        labels[count] = formatName(labels[count])
+
+        #setting up our data arrays
+        total.append(0)
+        totalFromAthens.append(0)
+        dTotal.append(0)
+
+        # get the amounts from athens added up
+        for i in range(len(columns[AMOUNT])):
+
+            # county and amount import, with NaN protection on the amount
+            county = str(columns[COUNTY][i])
+
+            if pandas.isna(columns[AMOUNT][i]):
+                amount = 0
+            else:
+                amount = str(columns[AMOUNT][i])
+
+                if amount[0] == "$": # remove $ if it has one
+                    amount = amount[1:len(amount)]
+                
+                amount = amount.split(",")
+                temp = ""
+                for i in amount:
+                    temp += i
+                amount = temp
+
+                amount = float(amount)
+
+            # format it to 5 digits if it's longer than that
+            if len(county) > 5:
+                county = county[0:5]
+
+            # add amount to total if county is athens
+            if county == "45701":
+                totalFromAthens[count] += amount
+                dTotal[count] += 1
+
+            total[count] += amount
+
+        count += 1
+
+    # make the stacked bar chart
     fig, ax = plot.subplots()
-    ax.pie(cleanAmountList, explode=explode, labels=cleanDonorList, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-    ax.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.bar(labels, totalFromAthens, 0.35, label = "Money from Athens")
 
-    #plot.show()
+    ax.set_ylabel("Amount Donated")
+    ax.set_title("Amounts Donated from Athens")
+    ax.legend()
 
-    fileName = fileName.split("\\")
-    candidateName =  fileName[len(fileName) - 1].split()
-    candidateName = candidateName[1]
-    fileName = candidateName + "-top-donor" + ("-othersTrue" if includeOthers else "") + ".png"
+    # file name, change the subdirectory of out
+    fileName = "../out/donations-from-athens/"
+    for file in files:
+        fileName += file.split("/")[3]
+        fileName += "-vs-"
+
+    fileName = fileName[0:len(fileName) - 4]
 
     plot.savefig(fileName)
-    """
--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ __author__ = "Jude Shreffler"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import time
 import TopDonor
 import AthensVsOthers as athens
 
 def main():
+    startTime = time.time()
+
     """ THESE NEED EDITED TO BE LINUX PATHS
     TopDonor.run("SPREADSHEETS\DATE FOUND_ 3-31-22\GOV RACES\contributions\(SOS) BLYSTONE CONTRIB - 1_31_22 .xlsx", False)
     TopDonor.run("SPREADSHEETS\DATE FOUND_ 3-31-22\GOV RACES\contributions\(SOS) CRANLEY CONTRIB - 2_16_22 .xlsx")
@@ -18,8 +21,17 @@ def main():
     TopDonor.run("SPREADSHEETS\DATE FOUND_ 3-31-22\GOV RACES\contributions\(SOS) WHALEY CONTRIB - 2_12_22 .xlsx")
     """
 
-    athens.run("./SPREADSHEETS/DATE FOUND_ 3-31-22/GOV RACES/contributions/(SOS) BLYSTONE CONTRIB - 1_31_22 .xlsx")
+    athens.run(["../data/attorney-race/jeff-crossman/jeff-crossman-contributions-(4-21-22).xlsx", "../data/attorney-race/dave-yost/dave-yost-contributions-(4-21-22).xlsx"  ])
+    athens.run(["../data/auditor-race/keith-faber/keith-faber-contributions-(2-16-22).xlsx", "../data/auditor-race/taylor-sappington/taylor-sappington-contributions-(3-31-22).xlsx"])
+    athens.run(["../data/gov-races/jim-renacci/jim-renacci-contributions-(1-31-22).xlsx", "../data/gov-races/john-cranley/john-cranley-contributions-(2-16-22).xlsx", "../data/gov-races/mike-dewine/mike-dewine-contributions-(2-10-22).xlsx", "../data/gov-races/nan-whaley/nan-whaley-contributions-(2-12-22).xlsx"])
+    athens.run(["../data/house-races/jay-edwards/jay-edwards-contributions-(3-31-22).xlsx", "../data/house-races/rhyan-goodman/rhyan-goodman-contributions-(4-26-22).xlsx"])
+    athens.run(["../data/sec-of-state-race/chelsea-clark/chelsea-clark-contributions-(4-21-22).xlsx", "../data/sec-of-state-race/frank-larose/frank-larose-contributions-(4-21-22).xlsx", "../data/sec-of-state-race/john-adams/john-adams-contributions-(4-21-22).xlsx"])
+    athens.run(["../data/sen-races/jane-timken/jane-timken-contributions-(4-21-22).xlsx", "../data/sen-races/jd-vance/jd-vance-contributions-(4-21-22).xlsx", "../data/sen-races/josh-mandel/josh-mandel-contributions-(4-21-22).xlsx", "../data/sen-races/mark-pukita/mark-pukita-contributions-(4-21-22).xlsx", "../data/sen-races/matt-dolan/matt-dolan-contributions-(4-21-22).xlsx", "../data/sen-races/morgan-harper/morgan-harper-contributions-(4-21-22).xlsx", "../data/sen-races/neil-patel/neil-patel-contributions-(4-21-22).xlsx", "../data/sen-races/tim-ryan/tim-ryan-contributions-(4-21-22).xlsx"])
+    athens.run(["../data/treasurer-race/robert-sprague/robert-sprague-contributions-(4-21-22).xlsx", "../data/treasurer-race/scott-schertzer/scott-schertzer-contributions-(4-21-22).xlsx"])
 
+    endTime = time.time()
+
+    print(f"It took {endTime-startTime:.2f} seconds to generate these charts")
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
